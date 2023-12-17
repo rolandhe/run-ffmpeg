195d4c15cc0d1742e790a939d8635528068f3792
hexiufeng <rolandhe@qq.com>
2023-12-17 23:19:34 +0800

    add get mem data info

--- quick_tool.c
+++ quick_tool.c
@@ -5,6 +5,8 @@
 #include "run_ffmpeg.h"
 #include <stdlib.h>
 #include <string.h>
+#include <libavutil/log.h>
+
 typedef struct _mem_data {
     char * buffer;
     int size;
@@ -25,8 +27,12 @@ int64_t new_input_mem(char * input_data,int64_t input_len,int copy){
 
 int64_t new_output_mem(){
     mem_data * p_data = malloc(sizeof(mem_data));
-    memset(p_data,0,sizeof(mem_data));
-    return (int64_t )p_data;
+    p_data->buffer = NULL;
+    p_data->size = 0;
+    int64_t  ret = (int64_t )p_data;
+
+    av_log(NULL, AV_LOG_INFO, "new_output_mem:0x%lx\n",ret);
+    return ret;
 }
 
 int64_t mem_data_len(int64_t point){
@@ -34,7 +40,23 @@ int64_t mem_data_len(int64_t point){
         return 0;
     }
     mem_data * p = (mem_data*)point;
+    av_log(NULL, AV_LOG_INFO, "get data len:%d\n",p->size);
+
+//    FILE * f = fopen("/home/xiufeng/github/ffmpego/inc.pcm","wb");
+//    fwrite(p->buffer,1,p->size,f);
+//    fclose(f);
     return (int64_t)p->size;
+}
+
+int8_t * get_mem_info(int64_t point,int * data_len){
+    *data_len = 0;
+    if(!point){
+        return NULL;
+    }
+    mem_data * p = (mem_data*)point;
+    av_log(NULL, AV_LOG_INFO, "get data len:%d\n",p->size);
+    *data_len = p->size;
+    return (int8_t*)p->buffer;
 }
 
 void free_mem(int64_t point,int release_data){
--- run_ffmpeg.h
+++ run_ffmpeg.h
@@ -18,6 +18,7 @@ int quick_duration(char * trace_id,char *cmd, int64_t * p_duration);
 int64_t new_input_mem(char * input_data,int64_t input_len,int copy);
 int64_t new_output_mem();
 int64_t mem_data_len(int64_t point);
+int8_t * get_mem_info(int64_t point,int * data_len);
 void free_mem(int64_t point,int release_data);
 
 #endif //RUN_FFMPEG_PARSE_CMD_H
